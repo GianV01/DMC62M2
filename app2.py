308b25ded5dcba56ce97c3b489d66319603be7e6
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-17 21:56:42 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,17 @@
 import streamlit as st
+import pandas as pd
+
+st.title ("Manejo de DataFrame")
+st.slidebar.title ("Herramientas")
+
+archivo = st.sliderbar.flie_uplowder("Selecciona tu archibo a cargar")
+if archivo is not None:
+  st.write ("Su archivo ha sido cargado exitosamente")
+  
+  if archivo.name.endswith(".csv"):
+    datos = pd.read_csv(archivo)
+  if archivo.name.endswith(".xlsx"):
+    datos = pd.read_excel(archivo)
+  st.write (datos)
+else:
+  st.write("Carga tu archivo")
